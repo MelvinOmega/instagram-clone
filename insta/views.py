bed361525eccf301e945a4271e46e502a34601f0
Melvin Omega <melvinomega151@gmail.com>
2021-10-17 16:39:50 +0300

    add postlistview and post create view

--- insta/views.py
+++ insta/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .forms import PostForm
 from .models import post
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 
 # Create your views here.
 
 
 class PostListView(ListView):
-    template_name = "insta/post_list.html"
+    template_name = "gram/post_list.html"
     queryset = post.objects.all()
     context_object_name = "posts"
+
+class PostCreateView(CreateView):
+        template='gram/post_create.html'
+        from_class = PostForm
+        queryset = post.objects.all()
