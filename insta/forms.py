39bc0b18b4a6950eaf98fae1732514d26a363e19
Melvin Omega <melvinomega151@gmail.com>
2021-10-17 16:39:07 +0300

    add forms file

--- insta/forms.py
+++ insta/forms.py
@@ -1,8 +1,24 @@
-from django import forms
+from django import forms, template
+from django.db.models import fields
+from django.db.models.query import QuerySet
 from .models import Post
-from crispy_forms.helper import Formhelper
+from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Field
 
 
 class PostForm(forms.Modelform):
-    helper = Formhelper()
+    helper = FormHelper()
+    helper.form_method = 'POST'
+    helper.add_input(Submit('Post','Post', css_class='btn-primary'))
+
+
+    class Meta:
+        model = Post
+        fields = [
+            'image',
+            'caption'
+        ]
+
+
+
+
