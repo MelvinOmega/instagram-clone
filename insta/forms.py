37dac6975249ee19780a8e1ad42ee22d44389fbe
Melvin Omega <melvinomega151@gmail.com>
2021-10-18 07:49:39 +0300

    created superuser

--- insta/forms.py
+++ insta/forms.py
@@ -1,23 +1,23 @@
-from django import forms, template
-from django.db.models import fields
-from django.db.models.query import QuerySet
-from .models import Post
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout, Field
+# from django import forms, template
+# from django.db.models import fields
+# from django.db.models.query import QuerySet
+# from .models import Post
+# from crispy_forms.helper import FormHelper
+# from crispy_forms.layout import Submit, Layout, Field
 
 
-class PostForm(forms.Modelform):
-    helper = FormHelper()
-    helper.form_method = 'POST'
-    helper.add_input(Submit('Post','Post', css_class='btn-primary'))
+# class PostForm(forms.Modelform):
+#     helper = FormHelper()
+#     helper.form_method = 'POST'
+#     helper.add_input(Submit('Post','Post', css_class='btn-primary'))
 
 
-    class Meta:
-        model = Post
-        fields = [
-            'image',
-            'caption'
-        ]
+#     class Meta:
+#         model = Post
+#         fields = [
+#             'image',
+#             'caption'
+#         ]
 
 
 
