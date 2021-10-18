e56204d7d193c1731ed997279758e2ac0216b10a
Melvin Omega <melvinomega151@gmail.com>
2021-10-18 11:24:44 +0300

    add register

---
+++ insta/urls.py
@@ -0,0 +1,10 @@
+# from django import path, include
+# from .views import PostListView, PostCreateView
+
+# app_name = 'insta'
+
+# urlpatterns = [
+
+#     path('', PostListView.as_view(), name='post_list'),
+#     path('new/', PostCreateView.as_view(), name = 'post_create')
+# ]
--- insta/views.py
+++ insta/views.py
@@ -1,17 +1,23 @@
-from django.shortcuts import render
-from .forms import PostForm
-from .models import post
-from django.views.generic import ListView, CreateView
+# from django.shortcuts import render
+# from .forms import PostForm
+# from .models import post
+# from django.views.generic import ListView, CreateView
 
-# Create your views here.
+# # Create your views here.
 
 
-class PostListView(ListView):
-    template_name = "gram/post_list.html"
-    queryset = post.objects.all()
-    context_object_name = "posts"
+# class PostListView(ListView):
+#     template_name = "gram/post_list.html"
+#     queryset = post.objects.all()
+#     context_object_name = "posts"
 
-class PostCreateView(CreateView):
-        template='gram/post_create.html'
-        from_class = PostForm
-        queryset = post.objects.all()
+# class PostCreateView(CreateView):
+#         template='gram/post_create.html'
+#         from_class = PostForm
+#         queryset = post.objects.all()
+
+
+#         def form_valid(self, form):
+#             print(form.cleaned_data)
+#             form.instance.author = self.request.user
+#             return super().form_valid(form)
