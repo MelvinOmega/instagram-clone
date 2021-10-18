d66a8ac2e9acc2d38f6eb981bf921e37c6f67494
Melvin Omega <melvinomega151@gmail.com>
2021-10-18 11:28:21 +0300

    add class post

--- insta/models.py
+++ insta/models.py
@@ -12,5 +12,5 @@ class Post(models.Model):
     caption = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
 
-    # def __str__(self):
-    #     return self.caption
+    def __str__(self):
+        return self.caption
