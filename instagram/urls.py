74a43e11035d0c8ad4360ec25ede99053abb29c2
Melvin Omega <melvinomega151@gmail.com>
2021-10-19 16:35:22 +0300

    add urls pattern

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# from django.contrib import admin
+# from django.conf.urls.static import static
+# from django.conf import Settings, settings
+# from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('insta.urls'))
-]
+] 
